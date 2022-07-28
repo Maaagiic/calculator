b52396f335a66f2e175f42b308f4313f9e0ee9fd
Maaagiic <denis.odigaev@mail.ru>
2022-07-29 01:41:47 +0300

    Наброски дизайна

--- calculator/MainWindow.xaml.cs
+++ calculator/MainWindow.xaml.cs
@@ -15,14 +15,91 @@ using System.Windows.Shapes;
 
 namespace calculator
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public double x;
+        public double y;
+        public char c;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox1.Text = "1";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            TextBox1.Text = "2";
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            TextBox1.Text = "3";
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            TextBox1.Text = "4";
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            TextBox1.Text = "5";
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            TextBox1.Text = "6";
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            TextBox1.Text = "7";
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            TextBox1.Text = "8";
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            TextBox1.Text = "9";
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            TextBox1.Text = "0";
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_11(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_12(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_13(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_14(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
