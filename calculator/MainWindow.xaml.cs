4b74d6f49298bcbed0efa69f252feac02c972d30
Maaagiic <denis.odigaev@mail.ru>
2022-07-30 17:20:43 +0300

    /

--- calculator/MainWindow.xaml.cs
+++ calculator/MainWindow.xaml.cs
@@ -67,57 +67,115 @@ namespace calculator
 
         private void Button_Click_10(object sender, RoutedEventArgs e)
         {
-            x = Convert.ToDouble(TextBox1.Text);
-            c = '+';
-            TextBox1.Text = "";
+            try
+            {
+                x = Convert.ToDouble(TextBox1.Text);
+                c = '+';
+                TextBox1.Text = "";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Введите число", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            }
         }
 
         private void Button_Click_11(object sender, RoutedEventArgs e)
         {
-            x = Convert.ToDouble(TextBox1.Text);
-            c = '-';
-            TextBox1.Text = "";
+            try
+            {
+                x = Convert.ToDouble(TextBox1.Text);
+                c = '-';
+                TextBox1.Text = "";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Введите число", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            }
         }
 
         private void Button_Click_12(object sender, RoutedEventArgs e)
         {
-            x = Convert.ToDouble(TextBox1.Text);
-            c = '*';
-            TextBox1.Text = "";
+            try
+            {
+                x = Convert.ToDouble(TextBox1.Text);
+                c = '*';
+                TextBox1.Text = "";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Введите число", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            }
         }
 
         private void Button_Click_13(object sender, RoutedEventArgs e)
         {
-            x = Convert.ToDouble(TextBox1.Text);
-            c = '/';
-            TextBox1.Text = "";
-        }
 
-        private void Button_Click_14(object sender, RoutedEventArgs e)
-        {
-            y = Convert.ToDouble(TextBox1.Text);
-            TextBox1.Text = "";
-            switch (c)
             {
-                case '+':
-                    TextBox1.Text = Convert.ToString(x + y);
-                    break;
-                case '-':
-                    TextBox1.Text = Convert.ToString(x - y);
-                    break;
-                case '/':
-                    TextBox1.Text = Convert.ToString(x / y);
-                    break;
-                case '*':
-                    TextBox1.Text = Convert.ToString(x * y);
-                    break;
-
-
+                try
+                {
+                    x = Convert.ToDouble(TextBox1.Text);
+                    c = '/';
+                    TextBox1.Text = "";
+                }
+                catch (Exception)
+                
+                    MessageBox.Show("Введите число", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
 
             }
 
 
+
+
         }
+
+        private void Button_Click_14(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                y = Convert.ToDouble(TextBox1.Text);
+                TextBox1.Text = "";
+                switch (c)
+                {
+                    case '+':
+                        TextBox1.Text = Convert.ToString(x + y);
+                        break;
+                    case '-':
+                        TextBox1.Text = Convert.ToString(x - y);
+                        break;
+                    case '/':
+                        if (y == 0)
+                        {
+                            MessageBox.Show("На ноль делить нельзя!", "Error", MessageBoxButton.OK);
+                        }
+                        else
+                        {
+                            TextBox1.Text = Convert.ToString(x / y);
+
+                        }
+                        break;
+
+                    case '*':
+                        TextBox1.Text = Convert.ToString(x * y);
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Введите выражение", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
+
+
+
+        }
+
+
+
 
         private void Button_Click_15(object sender, RoutedEventArgs e)
         {
@@ -133,51 +191,112 @@ namespace calculator
 
         private void Button_Click_17(object sender, RoutedEventArgs e)
         {
-            x = Convert.ToDouble(TextBox1.Text);
-            TextBox1.Text = Convert.ToString(Math.Pow(x, 2));
+            try
+            {
+                x = Convert.ToDouble(TextBox1.Text);
+                TextBox1.Text = Convert.ToString(Math.Pow(x, 2));
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Введите значение для вычисления", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
         }
 
         private void Button_Click_18(object sender, RoutedEventArgs e)
         {
-            x = Convert.ToDouble(TextBox1.Text);
-            TextBox1.Text = Convert.ToString(Math.Sqrt(x));
+            try
+            {
+                x = Convert.ToDouble(TextBox1.Text);
+                TextBox1.Text = Convert.ToString(Math.Sqrt(x));
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Введите значение для вычисления", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
         }
 
         private void Button_Click_19(object sender, RoutedEventArgs e)
         {
-            x = Convert.ToDouble(TextBox1.Text);
-            TextBox1.Text = Convert.ToString(Math.Sin((x * Math.PI) / 180));
+            try
+            {
+                x = Convert.ToDouble(TextBox1.Text);
+                TextBox1.Text = Convert.ToString(Math.Sin((x * Math.PI) / 180));
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Введите значение для вычисления", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
         }
 
         private void Button_Click_20(object sender, RoutedEventArgs e)
         {
-            x = Convert.ToDouble(TextBox1.Text);
-            TextBox1.Text = Convert.ToString(Math.Cos((x * Math.PI) / 180));
+            try
+            {
+                x = Convert.ToDouble(TextBox1.Text);
+                TextBox1.Text = Convert.ToString(Math.Cos((x * Math.PI) / 180));
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Введите значение для вычисления", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
         }
 
         private void Button_Click_21(object sender, RoutedEventArgs e)
         {
-            x = Convert.ToDouble(TextBox1.Text);
-            TextBox1.Text = Convert.ToString(Math.Tan((x * Math.PI) / 180));
+            try
+            {
+                x = Convert.ToDouble(TextBox1.Text);
+                TextBox1.Text = Convert.ToString(Math.Tan((x * Math.PI) / 180));
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Введите значение для вычисления", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
         }
 
         private void Button_Click_22(object sender, RoutedEventArgs e)
         {
-            x = Convert.ToDouble(TextBox1.Text);
-            TextBox1.Text = Convert.ToString((Math.Cos((x * Math.PI) / 180)) / Math.Sin((x * Math.PI) / 180));
+            try
+            {
+                x = Convert.ToDouble(TextBox1.Text);
+                TextBox1.Text = Convert.ToString((Math.Cos((x * Math.PI) / 180)) / Math.Sin((x * Math.PI) / 180));
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Введите значение для вычисления", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
         }
 
         private void Button_Click_23(object sender, RoutedEventArgs e)
         {
-            string s = TextBox1.Text;
-            s = s.Substring(0, s.Length - 1);
-            TextBox1.Text = s;
+            try
+            {
+                string s = TextBox1.Text;
+                s = s.Substring(0, s.Length - 1);
+                TextBox1.Text = s;
+            }
+            catch (Exception)
+            {
+
+                return;
+            }
+
 
         }
 
-        private void Button_ImageFailed(object sender, ExceptionRoutedEventArgs e)
-        {
 
-        }
     }
 }
